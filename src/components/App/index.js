2e4a8ab0c87d3321c5baeae655c14d341cb3e89b
Lucas <lucas.santos@mundowap.com.br>
2022-12-21 21:10:02 -0300

    chore: add fonts

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Onbooarding Component</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  title: {
+    fontFamily: 'Roboto-Medium',
+  },
+})
